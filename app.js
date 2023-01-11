5fa2ca328d3b9bb9941fc11fed171e0ab30053bc
jesperi.kuula <jeppe.kuula@gmail.com>
2023-01-11 16:00:46 +0200

    applying innerHTML to Bank-section

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 const laptopsElement = document.getElementById("laptops");
+const personElement = document.getElementById("person");
+const bankBalanceElement = document.getElementById("bankBalance");
+const loanElement = document.getElementById("loanButton");
+const workElement = document.getElementById("workButton");
+const bankElement = document.getElementById("bankButton");
+const buyNowElement = document.getElementById("buyNowButton");
 
+
+let person = {
+    name: "Joe Banker",
+    bankBalance: 2040,
+    loan: 0,
+    workBalance: 0
+};
+personElement.innerHTML = person.name;
+bankBalanceElement.innerHTML = person.bankBalance;
 let laptops = [];
 
 fetch("https://hickory-quilled-actress.glitch.me/computers")
